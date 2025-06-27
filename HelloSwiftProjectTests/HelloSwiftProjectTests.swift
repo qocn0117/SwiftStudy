5f9c6cd8b7afef46a4bb100feddcb92d30734823
qocn0117 <alswo8182@naver.com>
2025-06-28 02:24:16 +0900

    첫 커밋 HelloSwiftProject 초기화

---
+++ HelloSwiftProjectTests/HelloSwiftProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftProjectTests.swift
+//  HelloSwiftProjectTests
+//
+//  Created by 정민재 on 6/27/25.
+//
+
+import Testing
+@testable import HelloSwiftProject
+
+struct HelloSwiftProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
